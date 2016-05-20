37884ec01048f82ed6338183976ef5bcde54162e
lucas barbula <lucas.barbula@gmail.com>
2016-05-20 07:05:02 -0600

    final changes

--- public/classes.js
+++ public/classes.js
@@ -3,52 +3,68 @@ $(document).ready(function() {
     $.get('areas.json').done(function(data) {
         var areas = data.features;
         for (var i = 0; i < areas.length; i++) {
-            $(".area-list").append("<a href='#'><p id ='areas'>" + areas[i].properties.name + "</p></a>")
+            $(".area-list").append("<option>" + areas[i].properties.name + "</option>")
         }
+        $('.area-list').change(function() {
+            var chosenArea = $('.area-list option:selected').text();
+            //for (var i = 0; i < areas.length; i++) {
+                if (chosenArea == areas[i].properties.name) {
 
-        var json = localStorage.getItem('routes')
-        var localData;
-        if (json) {
-            localData = JSON.parse(json)
-        }
-        $.each(localData, function(key, value) {
-            $('.area-list').append('<a href="#"><p>' + value.area + '</p></a>')
-            $('.area-list > a').on('click', function() {
-                var selected = $(this).text()
-
-                if (selected.toLowerCase() == value.area.toLowerCase()) {
-                    alert("you got it")
-                    $('#pop-up').append('<p>' + value.routeName + '</p><br>' + '<p>' + value.beta + '</p><br>' + value.grade + '</p>')
+                    $('#pop-up').append('<p>' + areas[i].properties.name + '</p><br>' + '<p>' + value.beta + '</p><br>' + value.grade + '</p>')
                     $('#pop-up').toggle()
 
-                } else  {
+                } else {
                     $('#pop-up').append('<p>' + "No routes for this area" + '</p>')
                     $('#pop-up').toggle()
                     alert("whoops")
 
                 }
-                console.log(selected)
-            })
-
+            //}
         })
+    })
+    var json = localStorage.getItem('routes')
+    var localData;
+    if (json) {
+        localData = JSON.parse(json)
+    }
+    $.each(localData, function(key, value) {
+            $(".area-list").append("<option>" + value.area + "</option>")
+            $('.area-list').change(function() {
+            var selected = $('.area-list option:selected').text();
+
+            if (selected == value.area) {
+
+                $('#pop-up').append('<p>' + 'Route Name:' + " " + value.routeName + '</p><br>' + '<p>' + 'Beta:' + " " + value.beta + '</p><br>' + 'Grade:' + " " + value.grade + '</p>')
+                $('#pop-up').toggle()
+
+            } else {
+                $('#pop-up').append('<p>' + "No routes for this area" + '</p>')
+                $('#pop-up').toggle()
+                alert("whoops")
+
+            }
+            console.log(selected)
+        })
+
     })
 
 
-    //Local Storage
-    function getRoutes() {
-        var routes = localStorage.getItem('routes')
-        if (routes) {
-            return JSON.parse(routes)
-        } else {
-            return []
-        }
-    }
 
-    function addRoute(route) {
-        var routes = getRoutes()
-        routes.push(route)
-        var jsonStrR = JSON.stringify(routes)
-        localStorage.setItem('routes', jsonStrR)
+//Local Storage
+function getRoutes() {
+    var routes = localStorage.getItem('routes')
+    if (routes) {
+        return JSON.parse(routes)
+    } else {
+        return []
     }
+}
+
+function addRoute(route) {
+    var routes = getRoutes()
+    routes.push(route)
+    var jsonStrR = JSON.stringify(routes)
+    localStorage.setItem('routes', jsonStrR)
+}
 
 })
